df2e806967d491453ee9593725f3cff4f1c5f5d5
WagnerBenjamin <benjaminwagner@outlook.com>
2023-09-27 17:16:16 -0300

    borre endpoint

--- controllers/demo.js
+++ controllers/demo.js
@@ -4,10 +4,6 @@ const { getAuthFromClientCredentials } = require('../controllers/spotify-auth.js
 
 
 
-const helloWorld = (req = request, res = response) => {
-  res.send('Pagina raiz')
-} 
-
 const getArtist = async (req = request, res = response) => {    
   const access_token = await getAuthFromClientCredentials();    
   
@@ -110,10 +106,7 @@ const getArtistAlbums = async (req = request, res = response) => {
       res.status(404);
       console.error('Error al obtener datos de los albumes:', error);
     });
-  
-
-
-
+}
 
 const getAlbumesTracks = async (req = request, res = response) => {
   const access_token = await getAuthFromClientCredentials();
@@ -139,42 +132,13 @@ const getAlbumesTracks = async (req = request, res = response) => {
 
 }
 
-const getAudiobook = async (req = request, res = response) => {
-  const access_token = await getAuthFromClientCredentials();
-
-  const config = {
-    headers: {
-      'Authorization': `Bearer ${access_token}`
-    }
-  };
-
-  const audioBook_id = req.params["id"];
-
-  axios.get(`https://api.spotify.com/v1/audiobooks/${audioBook_id}`, config)
-  .then((response) => {
-    const audioBookData = response.data;
-    console.log('Datos del audiobook:', audioBookData);
-    res.status(200).json(audioBookData);
-  })
-  .catch((error) => {
-    res.status(404);
-    console.error('Error al obtener datos del album: ', error);
-  })
-
-}
 
 
 module.exports = {
-  helloWorld,
   getArtist, 
   getPlaylistTracks,
   getGenresRecomendation,
-<<<<<<< HEAD
-=======
   getArtistAlbums,
->>>>>>> main
   getAlbumesTracks,
-  getAudiobook
-
 }
 
--- routes/demo.js
+++ routes/demo.js
@@ -1,20 +1,18 @@
 const { Router } = require('express');
 
-const {  helloWorld, getArtist, getPlaylistTracks,getGenresRecomendation,getArtistAlbums,getAlbumesTracks, getAudiobook  } = require('../controllers/demo');
+const {  helloWorld, getArtist, getPlaylistTracks,getGenresRecomendation,getArtistAlbums,getAlbumesTracks } = require('../controllers/demo');
 
 
 
 
 const rutas = Router();
 
-rutas.get('', helloWorld);
-
 rutas.get('/artist/:id', getArtist);
 rutas.get('/playlist/:id', getPlaylistTracks);
 rutas.get('/recommendations/available-genre-seeds', getGenresRecomendation);
 rutas.get('/artist/:id/albums', getArtistAlbums);
 rutas.get('/albums/:id', getAlbumesTracks);
-rutas.get('/audiolibros/:id', getAudiobook);
+
 
 
 module.exports = rutas;
