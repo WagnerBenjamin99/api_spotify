6891fc1137d7509e785bdca710d9b78bd0921325
WagnerBenjamin <benjaminwagner@outlook.com>
2023-09-27 22:39:24 -0300

    prueba 2

--- controllers/spotify-auth.js
+++ controllers/spotify-auth.js
@@ -6,6 +6,7 @@ const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const url = 'https://accounts.spotify.com/api/token';
 
 const getAuthFromClientCredentials = async () => {
+  console.log("PARAMETROS....", url, client_id, client_secret);
 
   try {
     const { data } = await axios.post(url, {
